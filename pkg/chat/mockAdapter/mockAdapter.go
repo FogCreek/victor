238e2e9d4a9f3c63fbaf71049da7157a698f8737
Michael Stewart <michaels@fogcreek.com>
2015-06-29 13:53:56 -0400

    Added methods and implemention for the mock chat adapter.
    This should tests to be written for victor handlers which can then mock a
    message being received as well as view the resulting sent message(s) from
    the handlers' output.

--- pkg/chat/mockAdapter/mockAdapter.go
+++ pkg/chat/mockAdapter/mockAdapter.go
@@ -8,44 +8,121 @@ import (
 
 var nextID = 0
 
+// init Registers the mockAdapter with the victor framework under the chat
+// adapter name "mockAdapter".
 func init() {
 	chat.Register("mockAdapter", func(r chat.Robot) chat.Adapter {
 		id := nextID
 		nextID++
 		return &MockChatAdapter{
-			id: strconv.Itoa(id),
+			robot:              r,
+			id:                 strconv.Itoa(id),
+			Sent:               make([]MockMessagePair, 0, 10),
+			IsPotentialUserRet: true,
+			ChannelNameRet:     "fake_channel",
+			UserRet: &chat.BaseUser{
+				UserName:  "Fake User",
+				UserID:    "UFakeUser",
+				UserIsBot: false,
+				UserEmail: "fake@example.com",
+			},
 		}
 	})
 }
 
+// MockChatAdapter provides an "empty" chat adapter which can be used to test
+// victor and/or victor handler functions. It stores all sent messages to an
+// exported array and allows certain function's returned values (GetUser,
+// IsPotentialUser, etc.) to be set.
 type MockChatAdapter struct {
-	id string
+	id                 string
+	robot              chat.Robot
+	Sent               []MockMessagePair
+	UserRet            chat.User
+	IsPotentialUserRet bool
 }
 
+// Receive mocks a message being received by the chat adapter.
+func (m *MockChatAdapter) Receive(mp chat.Message) {
+	m.robot.Receive(mp)
+}
+
+// Run does nothing as the mockAdapter does not connect to anything.
 func (m *MockChatAdapter) Run() error {
 	return nil
 }
 
-func (m *MockChatAdapter) Send(string, string) {
-	return
+// Send stores the given channelID and text to the exported array "Sent" as
+// a MockMessagePair.
+func (m *MockChatAdapter) Send(channelID, text string) {
+	m.Sent = append(m.Sent, MockMessagePair{
+		text:      text,
+		channelID: channelID,
+		isDirect:  false,
+	})
 }
 
-func (m *MockChatAdapter) SendDirectMessage(string, string) {
-	return
+// SendDirectMessage stores the given userID and text to the exported array
+// "Sent" as a MockMessagePair with the "IsDirect" flag set to true.
+func (m *MockChatAdapter) SendDirectMessage(userID, text string) {
+	m.Sent = append(m.Sent, MockMessagePair{
+		text:     text,
+		userID:   userID,
+		isDirect: true,
+	})
 }
 
+// Stop does nothing.
 func (m *MockChatAdapter) Stop() {
 	return
 }
 
+// ID returns the mockAdapter's ID.
 func (m *MockChatAdapter) ID() string {
 	return m.id
 }
 
+// GetUser returns the mockAdapter's set "UserRet" property which can be
+// set to any chat.User instance (default value has full name "Fake User").
 func (m *MockChatAdapter) GetUser(string) chat.User {
-	return nil
+	return m.UserRet
 }
 
+// IsPotentialUser returns the mockAdapter's set "IsPotentialUserRet" property
+// which has a default value of "true".
 func (m *MockChatAdapter) IsPotentialUser(string) bool {
-	return false
+	return m.IsPotentialUserRet
+}
+
+// MockMessagePair is used to store messages that are sent by chat handlers to
+// the mockAdapter instance.
+type MockMessagePair struct {
+	channelID,
+	userID,
+	text string
+	isDirect bool
+}
+
+// ChannelID returns the id of the channel that the sent message was intended
+// for. If this is a direct message then this will not be set.
+func (mp *MockMessagePair) ChannelID() string {
+	return mp.channelID
+}
+
+// Text returns the message's full text.
+func (mp *MockMessagePair) Text() string {
+	return mp.text
+}
+
+// UserID returns the id of the user that the message was intended for. This
+// will not be sent unless it is a direct message.
+func (mp *MockMessagePair) UserID() string {
+	return mp.userID
+}
+
+// IsDirect returns true if the message is direct and false otherwise. The
+// output of this can be used to determine whether ChannelID() or UserID are
+// relavent to this message.
+func (mp *MockMessagePair) IsDirect() bool {
+	return mp.isDirect
 }
